524f02537d002ed15e7a770852d3245a4bb90394
Otto Streifel <otto.streifel@gmail.com>
2025-05-18 15:02:24 -0700

    Test API

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODO
+- Product list
+- Product details page
+- Add to cart
+- Cart page
+- Checkout cart
+- Orders page
+- Order details page
+
+
+## Resources
+https://dummyjson.com/
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// TODO move this interface to its own file.
+interface Product {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  }
+  // TODO the other fields
+}
+
 function App() {
+  const [products, setProducts] = useState<Product[]|null>(null);
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then((response) => {
+        setProducts(response.products)
+      });
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +40,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {products ? products.map(product => 
+          <div>
+            {product.title}
+            {product.description}
+          </div>
+        ): <p>Loading...</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
